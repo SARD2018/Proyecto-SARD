7a57c5696d132cbd0483ad4d3e3510ba6661d872
SENA <SENA@root.FormacionSena.RED>
2018-11-14 10:56:06 -0500

    veterinaria

--- src/java/Controlador/Servlet_Veterinaria_1.java
+++ src/java/Controlador/Servlet_Veterinaria_1.java
@@ -5,27 +5,21 @@
  */
 package Controlador;
 
-import Modelo.GS_Veterinaria;
-import Modelo.Veterinaria_M;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author SENA
  */
-@WebServlet(name = "Servlet_Veterinaria", urlPatterns = {"/Servlet_Veterinaria"})
-public class Servlet_Veterinaria extends HttpServlet {
+@WebServlet(name = "Servlet_Veterinaria_1", urlPatterns = {"/Servlet_Veterinaria_1"})
+public class Servlet_Veterinaria_1 extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -41,51 +35,9 @@ public class Servlet_Veterinaria extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         
-        JOptionPane.showMessageDialog(null, "entra servlet");
-        
-        if (request.getParameter("R_Veterinaria")!=null) {
-            JOptionPane.showMessageDialog(null,"entra boton");
-            this.Insertar_Veterinaria(request, response);
+        if (request.getParameter("A")!=null) {
+        JOptionPane.showMessageDialog(null, "entra al if");
         }
-    }
-    
-     protected void Insertar_Veterinaria (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
-        String Nit,Nombre,Representante,Fecha,Direccion,Barrio,Telefono,Correo;
-        Nit = request.getParameter("Nit");
-        Nombre = request.getParameter("Nombre");
-        Representante= request.getParameter("Representante");
-        Fecha = request.getParameter("Fecha_Fundacion");
-        Direccion = request.getParameter("Direccion");
-        Barrio = request.getParameter("Barrio");
-        Telefono = request.getParameter("Telefono");
-        Correo = request.getParameter("Correo");
-        Part Foto = request.getPart("Foto");
-        String Nombre_F = Foto.getSubmittedFileName();
-        String Foto_Name = Nombre+"_"+Nombre_F;
-        
-        String url = "G:\\Nueva Carpeta (3)\\SARD\\Proyecto-SARD\\web\\Uploads\\"+Foto_Name;
-        String url2 = "Uploads\\"+Foto_Name;
-        
-        InputStream file= Foto.getInputStream();
-        File img=new File(url);
-        FileOutputStream sal=new FileOutputStream(img);
-        int num= file.read();
-        
-        while (num !=-1) {            
-            sal.write(num);
-            num= file.read();
-        }
-        
-        GS_Veterinaria GSC = new GS_Veterinaria(Nit, Nombre, Representante, Fecha, Direccion, Barrio, Telefono, Correo, url2);
-        Veterinaria_M veterinaria = new Veterinaria_M();
-        veterinaria.In_Veterinaria(GSC);    ;
-        
-        request.getRequestDispatcher("Registros_Administrador.jsp").forward(request, response);
-        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
